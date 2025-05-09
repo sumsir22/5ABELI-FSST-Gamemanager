25c9ef2527cf555790ef63d6f3a9d4d91612ac98
sheery <117809130+sumsir22@users.noreply.github.com>
2025-05-09 21:42:22 +0200

    Added Buttons and Classes
    Added some buttons, a listviewer and the classes Game and RawgApiClient

---
+++ Code/5ABELI_FSST_Gamemanager/5ABELI_FSST_Gamemanager/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5ABELI_FSST_Gamemanager
+{
+    public class Game
+    {
+        public string name { get; set; } = "";
+        public string genre { get; set; } = "";
+        public string platform { get; set; } = "";
+        public string developer { get; set; } = "";
+        public string publisher { get; set; } = "";
+        public string releaseDate { get; set; } = "";
+
+    }
+}
---
+++ Code/5ABELI_FSST_Gamemanager/5ABELI_FSST_Gamemanager/RawgApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;          //Added to get information from the API
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace _5ABELI_FSST_Gamemanager
+{
+    class RawgApiClient
+    {
+
+    }
+}
