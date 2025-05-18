161ed79f37ed31cca02c4c8771b93b1c370f0cfe
sheery <samir.el-farfar@student.htl-rankweil.at>
2025-05-18 21:39:05 +0200

    implemented api (not complete)

--- Code/5ABELI_FSST_Gamemanager/5ABELI_FSST_Gamemanager/RawgApiClient.cs
+++ Code/5ABELI_FSST_Gamemanager/5ABELI_FSST_Gamemanager/RawgApiClient.cs
@@ -12,6 +12,48 @@ namespace _5ABELI_FSST_Gamemanager
 {
     class RawgApiClient
     {
+        private readonly string _apiKey;
+        private readonly HttpClient _httpClient;
 
+        public RawgApiClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Gamelist> SearchGamesAsync(string query)
+        {
+            var url = $"https://api.rawg.io/api/games?key={_apiKey}&search={query}";
+            var response = await _httpClient.GetStringAsync(url);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            using var doc = JsonDocument.Parse(response);
+            var root = doc.RootElement;
+            var results = root.GetProperty("results");
+
+            var gameList = new Gamelist();
+
+            foreach (var item in results.EnumerateArray())
+            {
+                var name = item.GetProperty("name").GetString();
+                var release = item.TryGetProperty("released", out var releasedProp)
+                                ? releasedProp.GetString()
+                                : "Unbekannt";
+
+                string genre = "Unbekannt";
+                if (item.TryGetProperty("genres", out var genresProp) && genresProp.GetArrayLength() > 0)
+                {
+                    genre = genresProp[0].GetProperty("name").GetString();
+                }
+
+                gameList.add(name, genre, release); // Add the game to the list
+            }
+
+            return gameList;
+        }
     }
 }
